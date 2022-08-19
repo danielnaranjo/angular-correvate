c203d3a70ef4ed4d46362f976144973395990499
Daniel Naranjo <>
2022-08-18 22:06:25 -0300

    added material design

---
+++ src/app/shared/components/material/index.ts
@@ -0,0 +1,42 @@
+
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+
+export const MaterialModulesList = [
+    MatFormFieldModule,
+    MatCardModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    MatIconModule,
+    MatButtonModule,
+    MatRadioModule,
+    MatMenuModule,
+    MatDividerModule,
+    MatTooltipModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatSnackBarModule,
+    MatAutocompleteModule,
+    MatProgressSpinnerModule,
+    MatBadgeModule,
+    MatGridListModule,
+    MatSidenavModule,
+];
