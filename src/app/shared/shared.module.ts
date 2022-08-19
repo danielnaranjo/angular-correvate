3e0671e34dabf28fa281be494c7e522e7f627515
Daniel Naranjo <>
2022-08-19 17:38:45 -0300

    remove useless

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PipesModule } from './pipes/pipes.module';
-import { LoadingComponent } from './components/loading/loading.component';
 
 @NgModule({
     declarations: [
-        LoadingComponent,
     ],
     exports: [
-        LoadingComponent,
     ],
     imports: [
         CommonModule,
